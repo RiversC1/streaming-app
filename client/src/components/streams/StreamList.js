46b736a296e2d527181d21611b42ee7d24f09485
Carlos Carrion <carloscrivers@gmail.com>
2018-12-26 20:19:35 -0500

    adding lodash and fetching a list to the StreamList component

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchStreams } from '../../actions';
 
+class StreamList extends Component {
+    componentDidMount() {
+        this.props.fetchStreams();
+    }
 
-const StreamList = () => {
-    return <div>Stream List</div>;
+    renderList() {
+        return this.props.streams.map(stream => {
+            return (
+                <div className="item" key={stream.id}>
+                    <i className="large middle aligned icon camera" />
+                    <div className="content">
+                            {stream.title}
+                       <div className="description">
+                            {stream.description}
+                       </div>
+                    </div>
+                </div>
+            );
+        });
+    }
+
+    render() {
+        console.log(this.props.streams);
+        return (
+            <div>
+                <h2>Streams</h2>
+                <div className="ui celled list">{this.renderList()}</div>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return { streams: Object.values(state.streams) };
 };
 
-export default StreamList;
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
